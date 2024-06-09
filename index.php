bb690f7ecb2d58afa3a44cb6a3223d8f82a3cd8b
otstdgch <111216582+otstdgch@users.noreply.github.com>
2024-06-09 18:43:03 +0800

    Update and rename index.html to index.php

--- index.php
+++ index.php
@@ -1,13 +1,18 @@
 <!DOCTYPE html>
+
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>贵州小炒</title>
     <style>
+        *{
+            padding: 0%;
+            margin: 0%;
+        }
         /* 设置标题栏样式 */
         .title-bar {
-            background-color: #f5f5f5; /* 浅灰色 */
+            background-color: #ffffff; /* 浅灰色 */
             color: #000000; /* 黑色 */
             font-size: 18px; /* 字体大小，18px相当于18dp */
             padding: 10px 0; /* 上下10px，左右0px的内边距 */
@@ -22,7 +27,7 @@
 </head>
 <body>
     <!-- 标题栏 -->
-    <div class="title-bar">6号桌</div>
+    <div class="title-bar">欢迎您的到来--<?php $_GET["Tablenum"] ?>号桌</div>
     <!-- 页面内容 -->
     <div style="padding-top: 60px;"> <!-- 为标题栏留出空间 -->
         <!-- 这里可以添加其他页面内容 -->
